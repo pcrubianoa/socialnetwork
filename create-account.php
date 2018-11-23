14db7e8e7920f421152af4cdd868234c7714ef82
pcrubianoa <pcrubianoa@gmail.com>
2018-11-23 13:34:28 -0500

    Account Validation

--- create-account.php
+++ create-account.php
@@ -1,12 +1,38 @@
 <?php
-include('classes/DB.php');
-if (isset($_POST['createaccount'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $email = $_POST['email'];
-        DB::query('INSERT INTO users VALUES (null, :username, :password, :email)', array(':username'=>$username, ':password'=>$password, ':email'=>$email));
-        echo "Success!";
-}
+    include('classes/DB.php');
+
+    if (isset($_POST['createaccount'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $email = $_POST['email'];
+
+            if (!DB::query('SELECT username FROM users WHERE username=:username', array(':username' => $username))){
+
+                if (strlen($username) >= 3 && strlen($username) <= 32) {
+                    
+                    if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+
+                        if (strlen($password) >= 6 && strlen($password) <= 60) {
+
+                        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                            DB::query('INSERT INTO users VALUES (null, :username, :password, :email)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                            echo "Success!";
+                        }else{
+                            echo 'Invalid email';
+                        }
+                    }else{
+                        echo 'Invalid Password';
+                    }
+                    }else{
+                        echo 'Invalid username';
+                    }
+                }else{
+                    echo 'Invalid username';
+                }
+            }else{
+                echo 'User already exists!';
+            }
+    }
 ?>
 
 <h1>Register</h1>
